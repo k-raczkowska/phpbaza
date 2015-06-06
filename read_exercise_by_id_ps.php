fb51ec829ef81b99da41e0cd8413ae4af968859d
piotrszych <piotrszych@hotmail.com>
2015-06-06 20:39:37 +0200

    Zmieniono zapytanie, by uwzględniało interwał

--- read_exercise_by_id_ps.php
+++ read_exercise_by_id_ps.php
@@ -21,7 +21,7 @@ if (isset($_GET['id_cwiczenia'])) {
 	$id_cwiczenia = $_GET['id_cwiczenia'];
  
     // mysql update row with matched pid
-    $result = mysql_query("SELECT ilosc_wykonanych, id_cwiczenia FROM cwiczenie_uzytkownika WHERE id = $id_cwiczenia ");
+    $result = mysql_query("SELECT c.ilosc_wykonanych, c.id_cwiczenia, d.interwal FROM cwiczenie_uzytkownika c LEFT JOIN d_cwiczenie d ON c.id_cwiczenia = d.id WHERE c.id = $id_cwiczenia ");
 	//echo mysql_num_rows($result);
     
 	if (mysql_num_rows($result) > 0) {
@@ -34,6 +34,7 @@ if (isset($_GET['id_cwiczenia'])) {
         $cwiczenia_uzytkownika = array();
         $cwiczenia_uzytkownika["ilosc_wykonanych"] = $row["ilosc_wykonanych"];
         $cwiczenia_uzytkownika["id_cwiczenia"] = $row["id_cwiczenia"];
+		$cwiczenia_uzytkownika["interwal"] = $row["interwal"];
         // push single product into final response array
         array_push($response["cwiczenie_uzytkownika"], $cwiczenia_uzytkownika);
     }
